9d13d7d2552e046260b0a84550f6ac56c3ff5c5d
Markus Palomäki <markuspalomaki@gmail.com>
2022-08-03 15:44:34 +0300

    Fetch developers information as JSON

--- src/index.js
+++ src/index.js
@@ -5,14 +5,35 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const https = require("https");
+const axios = require("axios");
 
 const PORT = process.env.PORT || 80;
+
+// Error handler, if axios fails
+const errorHandler = (err, req, res, next) => {
+    console.log("Error" + next);
+    res.status(err.status || 400).send(err.message);
+}
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-    res.send("Front page");
+// Get user data from GitHub API
+app.get("/", async (req, res, next) => {
+    const options = {
+        method: "get",
+        url: "https://api.github.com/users/markspl",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+
+    axios(options).then(resp => {
+        res.send(resp.data);
+    }).catch(err => {
+        next(err);
+    });
 });
 
 // Not found fallback
@@ -20,6 +41,8 @@ app.get("*", (req, res) => {
     const notFound = "<p><span style=\"font-weight:bold; color: red\">404</span> Not Found</p><a href=\"/\">Go back</a>"
     res.status(404).send(notFound);
 });
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Up and running on port ${PORT}.`)
