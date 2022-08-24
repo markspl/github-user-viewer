416ee405020d58c2a9ce62694e2b9758db94a7ce
Markus Palomäki <markuspalomaki@gmail.com>
2022-08-24 23:22:17 +0300

    Fix loading badges not visible with populating array while creating it

--- src/components/PlaceholderBadges.jsx
+++ src/components/PlaceholderBadges.jsx
@@ -3,8 +3,8 @@
 import PlaceholderBadge from "./PlaceholderBadge";
 
 const PlaceholderBadges = ({ value }) => {
-    // Create 1-{value} element array
-    const placeholderArray = Array(Math.floor(Math.random() * value + 5));
+    // Create 1-{value} element array with "undefined" values
+    const placeholderArray = Array(Math.floor(Math.random() * value + 5)).fill();
 
     // Populate array
     const placeholders = placeholderArray.map((_, i) => {
